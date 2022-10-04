38dd5f8cd58e99fd8d3cfa1ef37c8002ebf99bbe
Vasily Yurchenko <vasily.yurchenko@gmail.com>
2022-10-04 12:07:35 +0300

    There was made name refactoring for atb_imx8mp_sodimm_voskhod1 platform.

--- board/atb/atb_imx8mp_sodimm_voskhod1/post-image.sh
+++ board/atb/atb_imx8mp_sodimm_voskhod1/post-image.sh
@@ -7,7 +7,7 @@ img_utils_src=${buildroot_home}/dl/imx-mkimage/git/
 img_utils=${buildroot_home}/output/imx-mkimage/
 output=${buildroot_home}/output
 images=${output}/images
-board="atb-imx8mp-som"
+board="atb-imx8mp-sodimm-voskhod1"
 soc=iMX8MP
 
 BUILD_DIR=${output}/build
@@ -33,8 +33,10 @@ cp ${buildroot_home}/output/images/lpddr4_pmu_train_1d_imem_202006.bin ${img_uti
 cp ${buildroot_home}/output/images/lpddr4_pmu_train_1d_dmem_202006.bin ${img_utils}/iMX8M
 cp ${buildroot_home}/output/images/lpddr4_pmu_train_2d_imem_202006.bin ${img_utils}/iMX8M
 cp ${buildroot_home}/output/images/lpddr4_pmu_train_2d_dmem_202006.bin ${img_utils}/iMX8M
-cp ${buildroot_home}/output/images/atb-imx8mp-som-voskhod1.dtb ${img_utils}/iMX8M/imx8mp-evk.dtb
-cp ${buildroot_home}/output/images/bl31.bin ${img_utils}/iMX8M
+cp ${buildroot_home}/output/images/atb-imx8mp-sodimm-voskhod1.dtb ${img_utils}/iMX8M/imx8mp-evk.dtb
+echo !!!!!!!!!!!!!!!!!!
+#cp ${buildroot_home}/output/images/bl31.bin ${img_utils}/iMX8M
+cp /home/user/drive/workspace/imx8m/imx-mkimage/iMX8M/bl31.bin ${img_utils}/iMX8M
 cp ${buildroot_home}/output/build/uboot-${board}/u-boot-nodtb.bin	${img_utils}/iMX8M
 cp ${buildroot_home}/output/build/uboot-${board}/tools/mkimage	${img_utils}/iMX8M/mkimage_uboot
 
@@ -47,7 +49,8 @@ cd ${buildroot_home}
 # Prepare all files for genimage
 cp ${img_utils}/iMX8M/usd_flash.bin ${images}
 cp ${images}/Image ${images}/linux
-cp ${images}/atb-imx8mp-som-voskhod1.dtb ${images}/dtb
+#cp ${images}/atb-imx8mp-sodimm-voskhod1.dtb ${images}/dtb
+cp /home/user//drive/workspace/buildroot_atb/output/build/linux-lf-5.10.y_var03/arch/arm64/boot/dts/freescale/imx8mp-var-som-symphony.dtb ${images}/dtb
 cp ${images}/rootfs.cpio.gz ${images}/rootfs
 ${output}/host/bin/mkimage -A arm -T ramdisk -C gzip -d ${output}/images/rootfs.cpio.gz ${output}/images/rootfs
 
