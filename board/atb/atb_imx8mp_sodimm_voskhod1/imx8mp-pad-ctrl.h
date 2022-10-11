b855b4d436e0dd7b9293970044ce3c98c544aa54
Vasily Yurchenko <vasily.yurchenko@gmail.com>
2022-10-11 14:44:55 +0300

    atb-imx8mp-sodimm-voskhod1.dts atb-imx8mp-sodimm.dtsi files were totaly refactored, named macro definitions is usedd instead values for pad configuration.

--- board/atb/atb_imx8mp_sodimm_voskhod1/imx8mp-pad-ctrl.h
+++ board/atb/atb_imx8mp_sodimm_voskhod1/imx8mp-pad-ctrl.h
@@ -7,30 +7,30 @@
 #define __DTS_IMX8MP_PADCONF_H
 
 /* Pull Select Field */
-#define PAD_PULL_DISABLE        (0x0 << 8)
-#define PAD_PULL_ENABLE         (0x1 << 8)
+#define PAD_PULL_DISABLE		(0x0 << 8)
+#define PAD_PULL_ENABLE		(0x1 << 8)
 
 /* Input Select Field */
-#define PAD_INPUT_CMOS          (0x0 << 7)
-#define PAD_INPUT_SCHMITT       (0x1 << 7)
+#define PAD_INPUT_CMOS		(0x0 << 7)
+#define PAD_INPUT_SCHMITT	(0x1 << 7)
 
 /* Pull Up / Down Config. Field */
-#define PAD_WEAK_PULL_DOWN      (0x0 << 6)
-#define PAD_WEAK_PULL_UP        (0x1 << 6)
+#define PAD_WEAK_PULL_DOWN	(0x0 << 6)
+#define PAD_WEAK_PULL_UP		(0x1 << 6)
 
 /* Open Drain Field */
-#define PAD_OPEN_DRAIN_DISABLE  (0x0 << 5)
-#define PAD_OPEN_DRAIN_ENABLE   (0x1 << 5)
+#define PAD_OPEN_DRAIN_DISABLE	(0x0 << 5)
+#define PAD_OPEN_DRAIN_ENABLE	(0x1 << 5)
 
 /* Slew Rate Field */
-#define PAD_SLOW_SLEW_RATE      (0x0 << 4)
-#define PAD_FAST_SLEW_RATE      (0x1 << 4)
+#define PAD_SLOW_SLEW_RATE	(0x0 << 4)
+#define PAD_FAST_SLEW_RATE	(0x1 << 4)
 
 /* Drive Strength Field */
-#define PAD_DSE_X1              (0x0 << 1)
-#define PAD_DSE_X2              (0x2 << 1)
-#define PAD_DSE_X3              (0x1 << 1)
-#define PAD_DSE_X4              (0x3 << 1)
+#define PAD_DSE_X1			(0x0 << 1)
+#define PAD_DSE_X2			(0x2 << 1)
+#define PAD_DSE_X4			(0x1 << 1)
+#define PAD_DSE_X6			(0x3 << 1)
 
 #endif /* __DTS_IMX8MP_PADCONF_H */
 
