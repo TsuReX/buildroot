9752369a74e85609e53a9784f4278bcd2a7f4747
Anton Ermakov <a.ermakov@atb-e.ru>
2022-12-08 13:16:26 +0300

    Reafctored to standard form.

--- board/atb/atb-imx8mq-smarc-congasmc1/pre-image.sh
+++ board/atb/atb-imx8mq-smarc-congasmc1/pre-image.sh
@@ -1,16 +1,37 @@
 #!/bin/sh
 
-buildroot_home=$(pwd)
-board=atb-imx8mq-smarc-congasmc1
+set -x
 
-cd ${buildroot_home}/output/target
+#
+# Common variables
+#
+BUILDROOT_HOME=$(pwd)
+BOARD_DIR="$(dirname $0)"
+BOARD_NAME="$(basename ${BOARD_DIR})"
+
+echo "argc = $#"
+echo "arg0 = $0"
+echo "arg1 = $1"
+echo "arg2 = $2"
+
+if ! [ $# == 2 ]; then
+    echo "Invalid arguments"
+    echo "The script requres the following arguments: path_to_images_folder linux_dtb_file_name.dtb"
+    exit -1
+fi
+
+TARGET=${1}
+
+cd ${TARGET}
 
 mkdir -p var/run
 mkdir -p var/lock
 mkdir -p var/log
 mkdir -p home
 
-#cp ${buildroot_home}/board/atb/${board}/inittab ${buildroot_home}/output/target/etc/
-cp ${buildroot_home}/board/atb/${board}/fstab ${buildroot_home}/output/target/etc/
-cp ${buildroot_home}/board/atb/${board}/S90mount ${buildroot_home}/output/target/etc/init.d/
-chmod 0755 ${buildroot_home}/output/target/etc/init.d/S90mount
+cd -
+
+#cp ${BUILDROOT_HOME}/board/atb/${BOARD_NAME}/inittab ${BUILDROOT_HOME}/output/target/etc/
+cp ${BUILDROOT_HOME}/board/atb/${BOARD_NAME}/fstab ${TARGET}/etc/
+cp ${BUILDROOT_HOME}/board/atb/${BOARD_NAME}/S90mount ${TARGET}/etc/init.d/
+chmod 0755 ${TARGET}/etc/init.d/S90mount
