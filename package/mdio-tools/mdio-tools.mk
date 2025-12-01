b2296d7f73106cafd53e9adc542105585be0868a
Vasily Yurchenko <vasily.yurchenko@gmail.com>
2025-12-01 06:46:45 +0000

    Changes: 		Versions of mdio-tools and strace packages were updated.

--- package/mdio-tools/mdio-tools.mk
+++ package/mdio-tools/mdio-tools.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-MDIO_TOOLS_VERSION = 1.2.0
-MDIO_TOOLS_SITE = https://github.com/wkz/mdio-tools/releases/download/$(MDIO_TOOLS_VERSION)
+MDIO_TOOLS_VERSION = 1.3.1
+MDIO_TOOLS_SITE = https://github.com/wkz/mdio-tools/releases/$(MDIO_TOOLS_VERSION)/download
 MDIO_TOOLS_LICENSE = GPL-2.0
 MDIO_TOOLS_LICENSE_FILES = COPYING
 MDIO_TOOLS_DEPENDENCIES = host-pkgconf libmnl
